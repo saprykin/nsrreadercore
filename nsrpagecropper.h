5320c351f65b70ad659101b2198be97595098bd7
Alexander Saprykin <xelfium@gmail.com>
2014-09-20 21:17:17 +0400

    Add documentation for NSRPageCropper

--- nsrpagecropper.h
+++ nsrpagecropper.h
@@ -1,19 +1,40 @@
-#ifndef NSRPAGECROPPER_H_
-#define NSRPAGECROPPER_H_
+#ifndef __NSRPAGECROPPER_H__
+#define __NSRPAGECROPPER_H__
+
+/**
+ * @file nsrpagecropper.h
+ * @author Alexander Saprykin
+ * @brief Page cropper
+ */
 
 #include "nsrcroppads.h"
 
+/**
+ * @class NSRPageCropper nsrpagecropper.h
+ * @brief Page cropper class
+ */
 class NSRPageCropper
 {
 public:
+	/** Pixel order of image data */
 	enum NSRPixelOrder {
-		NSR_PIXEL_ORDER_RGB	= 0,
-		NSR_PIXEL_ORDER_ARGB	= 1,
-		NSR_PIXEL_ORDER_BGR	= 2,
-		NSR_PIXEL_ORDER_BGRA	= 3,
-		NSR_PIXEL_ORDER_RGBA	= 4
+		NSR_PIXEL_ORDER_RGB	= 0,	/**< 3-bytes RGB	*/
+		NSR_PIXEL_ORDER_ARGB	= 1,	/**< 4-bytes ARGB	*/
+		NSR_PIXEL_ORDER_BGR	= 2,	/**< 3-bytes BGR	*/
+		NSR_PIXEL_ORDER_BGRA	= 3,	/**< 4-bytes BGRA	*/
+		NSR_PIXEL_ORDER_RGBA	= 4	/**< 4-bytes RGBA	*/
 	};
 
+	/**
+	 * @brief Tries to detect crops for given image data
+	 * @param data Image data of the page.
+	 * @param order	Pixel order of image data.
+	 * @param width	Image width, in px.
+	 * @param height Image height, in px.
+	 * @param stride Image row stride, in bytes.
+	 * @param widthLimit Minimum image width, 0 for no limits.
+	 * @return Detected image crops.
+	 */
 	static NSRCropPads findCropPads (unsigned char *	data,
 					 NSRPixelOrder		order,
 					 int			width,
@@ -22,7 +43,8 @@ public:
 					 int			widthLimit);
 
 private:
+	/** Hidden constructor */
 	NSRPageCropper ();
 };
 
-#endif /* NSRPAGECROPPER_H_ */
+#endif /* __NSRPAGECROPPER_H__ */
