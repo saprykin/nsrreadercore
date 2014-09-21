a0adc5290330fc2a8627faf01df53aa691ba9a2b
Alexander Saprykin <xelfium@gmail.com>
2014-09-21 20:22:48 +0400

    Add documentation for global header

--- nsrreadercore_global.h
+++ nsrreadercore_global.h
@@ -1,12 +1,19 @@
-#ifndef NSRREADERCORE_GLOBAL_H_
-#define NSRREADERCORE_GLOBAL_H_
+#ifndef __NSRREADERCORE_GLOBAL_H__
+#define __NSRREADERCORE_GLOBAL_H__
+
+/**
+ * @file nsrreadercore_global.h
+ * @author Alexander Saprykin
+ * @brief Global library header
+ */
 
 #include <qglobal.h>
 
+/** @def NSRREADERCORE_SHARED Library export/import macro */
 #ifdef NSRREADERCORE_LIBRARY
 #  define NSRREADERCORE_SHARED	Q_DECL_EXPORT
 #else
 #  define NSRREADERCORE_SHARED	Q_DECL_IMPORT
 #endif
 
-#endif /* NSRREADERCORE_GLOBAL_H_ */
+#endif /* __NSRREADERCORE_GLOBAL_H__ */
